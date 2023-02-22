61eed20688dfb244e9ee9aecd07a5d26adfa617a
Marcin Kozlowski <marcinguy@gmail.com>
2023-02-22 09:44:36 +0100

    Fix

--- build/extension.py
+++ build/extension.py
@@ -21,18 +21,18 @@ def betterscan_scan1():
     cwd = os.getcwd()
     mount = cwd+":"+cwd
 
-    subprocess.call("docker run -e CODE_DIR -v "+mount+" -i scanmycode/scanmycode3-ce:worker-cli /bin/sh -c 'cd $CODE_DIR && git config --global --add safe.directory $CODE_DIR && checkmate init'", shell=True)
+    subprocess.call("docker run -e CODE_DIR -e LIC -v "+mount+" -i scanmycode/scanmycode3-ce:worker-cli /bin/sh -c 'cd $CODE_DIR && git config --global --add safe.directory $CODE_DIR && checkmate init'", shell=True)
 
 def betterscan_scan2():
     cwd = os.environ["CODE_DIR"]
     mount = cwd+":"+cwd
-    subprocess.call("docker run -e CODE_DIR -v "+mount+" -i scanmycode/scanmycode3-ce:worker-cli /bin/sh -c 'git config --global --add safe.directory $CODE_DIR && cd $CODE_DIR && checkmate git init'",  shell=True)
+    subprocess.call("docker run -e CODE_DIR -e LIC -v "+mount+" -i scanmycode/scanmycode3-ce:worker-cli /bin/sh -c 'git config --global --add safe.directory $CODE_DIR && cd $CODE_DIR && checkmate git init'",  shell=True)
 
 
 def betterscan_scan3():
     cwd = os.environ["CODE_DIR"]
     mount = cwd+":"+cwd
-    subprocess.call("docker run -e CODE_DIR -v "+mount+" -i scanmycode/scanmycode3-ce:worker-cli /bin/sh -c 'git config --global --add safe.directory $CODE_DIR && cd $CODE_DIR && checkmate git analyze --branch `git rev-parse --abbrev-ref HEAD` && checkmate issues html '",  shell=True)
+    subprocess.call("docker run -e CODE_DIR -e LIC -v "+mount+" -i scanmycode/scanmycode3-ce:worker-cli /bin/sh -c 'git config --global --add safe.directory $CODE_DIR && cd $CODE_DIR && checkmate git analyze --branch `git rev-parse --abbrev-ref HEAD` && checkmate issues html '",  shell=True)
 
 
 
